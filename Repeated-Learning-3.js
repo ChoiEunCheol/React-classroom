139251055d3eae727a00ddbfd88a585b89dca2fb
최은철 <wndqhrgkdlel@gmail.com>
2023-11-14 10:28:16 +0900

    class 방식으로 작성

---
+++ Repeated-Learning-3.js
@@ -0,0 +1,43 @@
+class TitleManager {
+  constructor(
+    tagname,
+    stateDataObject = { basicTitle: "기본 제목", newTitle: "새로운 제목" }
+  ) {
+    this._tagName = tagname;
+    this._stateDataObject = stateDataObject;
+  }
+
+  get tagname() {
+    return this._tagName;
+  }
+
+  set tagname(value) {
+    if (typeof value === "string" && value.trim() !== "") {
+      this._tagName = value;
+    } else {
+      throw new Error("유효한 태그 이름이어야 합니다.");
+    }
+  }
+  get stateData() {
+    return this._stateDataObject;
+  }
+  set stateDataObject(value) {
+    const isValidObject = (obj) => {
+      typeof obj === "object" && obj !== null;
+    };
+    if (isValidObject(value)) {
+      this._stateDataObject = value;
+    } else {
+      throw new Error("유효한 객체 이름이어야 합니다.");
+    }
+  }
+  updateTitle() {
+    this._stateDataObject.basicTitle = this._stateDataObject.newTitle;
+    this.updateTitle();
+  }
+}
+
+const test = new TitleManager("h1", {basicTitle: "기본 제목", newTitle: "새로운 제목"});
+
+console.dir(test);
+console.log(test.tagname);
